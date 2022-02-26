133fb4245811c38efa47137ee5ff1f4118218ae3
Sean Montgomery <seandmontgomery@gmail.com>
2022-02-26 12:59:18 -0500

    notes expand

--- website/static/js/display.js
+++ website/static/js/display.js
@@ -1,30 +1,34 @@
-const photoAlbumButton = document.getElementById('photo-album-open');
+$('.card-text-wrapper').click(function() {
+  clickToExapndCards($(this));
+});
 
-photoAlbumButton.addEventListener('click', function() {
+function clickToExapndCards($obj){
+  var clickedElement = $obj;
+  if (clickedElement.hasClass('expanded')) {
+    clickedElement.find('.card-text').hide('slow');
+    clickedElement.removeClass('expanded');
+  } else {
+     clickedElement.find('.card-text').show('slow');
+    clickedElement.addClass('expanded');
+  }
+  
+};
+
+const photoAlbumDropdown = document.getElementById('album-dropdown');
+
+photoAlbumDropdown.addEventListener('click', function() {
     // Get the data id that we identified in the element
-    let trailId = photoAlbumButton.getAttribute('data-trail-id-for-album');
+    let trailId = photoAlbumDropdown.getAttribute('data-trail-id-for-album');
     // Call the function we wrote with this data id as argument
     
     $.get(`/api/node/${trailId}`, (resp) => {
       let photoArray = resp.photo_array
-      // parse the json before the get?
-      // Loop through objects
-      // Make sure you handle an empty array
+      function addCode() { 
+      for (let i = 0; i < photoArray.length; i++) {
+          photoAlbumDropdown.innerHTML += "<h3>This is the text which has been inserted by JS {{ trail.trail_id }}</h3>";
+      }}
       // access URL with dot notation 
       console.log(photoArray)
-      
-      
-      
-
-      // use fslightbox...
-
-      //const lightbox = new FsLightbox();
-
-      // set up props, like sources, types, events etc.
-
-      // lightbox.props.sources = [];
-      // lightbox.props.onInit = () => console.log('Lightbox initialized!');
-      // lightbox.open();
 
     }
 
