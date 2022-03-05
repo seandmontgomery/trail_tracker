bb674c596fffec3325bb782d0c23faebd8c9b0b9
Sean Montgomery <seandmontgomery@gmail.com>
2022-03-04 21:20:05 -0500

    working notes and photo modals

--- website/static/js/display.js
+++ website/static/js/display.js
@@ -1,39 +1,35 @@
 // --------- NOTES MODAL-----------------------------
 
-// var notesModal = document.getElementById('notesModal');
-// notesModal.addEventListener('show.bs.modal', function (event) {
-//   var notesButton = event.relatedTarget
-//   var trailID = notesButton.getAttribute('data-modal')
+document.querySelectorAll('.notes').forEach(noteBtn => {
+  console.log(noteBtn);
+  noteBtn.addEventListener('click', function (event) {
+  
+    // var notesButton = event.relatedTarget
+    var trailID = noteBtn.getAttribute('data-modal')
+  
+    console.log(trailID);
 
-//   console.log(trailID);
-//   console.log(notesButton);
-
-//       $.get(`/api/notes-modal/${trailID}`, (resp) => {
-//       let trailTitle = resp.title
-//       let trailNotes = resp.notes
-//       console.log(trailTitle);
-//       console.log(trailNotes);
-
-//       var modalTitle = exampleModal.querySelector('.modal-title')
-//       var modalBody = exampleModal.querySelector('.modal-body')
-
-//       modalTitle.textContent = trailTitle;
-//       modalBody.textContent = trailNotes;
-//     })
-
-//   var modalTitle = exampleModal.querySelector('.modal-title')
-//   var modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-//   modalTitle.textContent = '${trailID} ${notesButton}';
-//   modalTitle.textContent = 'Hello modal title';
-// });
+        $.get(`/api/notes-modal/${trailID}`, (resp) => {
+        let trailTitle = resp.notes_data.title
+        let trailNotes = resp.notes_data.notes
+        console.log(trailTitle);
+        console.log(trailNotes);
+  
+        var modalTitle = document.querySelector('.modal-title')
+        var modalBody = document.querySelector('.modal-body')
+  
+        modalTitle.textContent = trailTitle;
+        modalBody.textContent = trailNotes;
+      })
+  });
+})
 
 // --------- PHOTO ALBUM -----------------------------
 
-const photoAlbumButton = document.getElementById('photo-album-open');
-
-photoAlbumButton.addEventListener('click', function() {
-    let trailId = photoAlbumButton.getAttribute('data-trail-id-for-album');
+document.querySelectorAll('.photo-album-open').forEach(btn => {
+  
+  btn.addEventListener('click', function() {
+    let trailId = btn.getAttribute('data-trail-id-for-album');
     
     $.get(`/api/node/${trailId}`, (resp) => {
       const photoArray = resp.photo_array
@@ -67,4 +63,50 @@ photoAlbumButton.addEventListener('click', function() {
     }
 
 )});
+})
+
+// photoAlbumButton.addEventListener('click', function() {
+//     let trailId = photoAlbumButton.getAttribute('data-trail-id-for-album');
+    
+//     $.get(`/api/node/${trailId}`, (resp) => {
+//       const photoArray = resp.photo_array
+//       console.log(photoArray)
+      
+//         // Loop through objects
+//         // access URL with dot notation 
+//         for(let i = 0; i < photoArray.length; i+=1){
+//           let currDiv;
+//           if(i === 0) {
+//             currDiv = document.createElement('div')
+//             currDiv.setAttribute('class', 'carousel-item active')
+//           } else {
+//             currDiv=  document.createElement('div')
+//             currDiv.setAttribute('class','carousel-item')
+//           }
+//            img = document.createElement('img')
+            
+//           const urlOfActiveDivsImg = photoArray[i].url
+//           img.setAttribute('class',"d-block w-100")
+//           img.setAttribute('alt',"first slide")
+//           img.setAttribute("src",urlOfActiveDivsImg);
+         
+//           currDiv.appendChild(img)
+        
+//           const carouselInner = document.querySelectorAll(".carousel-inner")[0]
+            
+//           carouselInner.appendChild(currDiv)
+              
+//         }
+//     }
+
+// )});
+
+$('#photoModal').on('hidden.bs.modal', function (e) {
+ const innerDiv = document.querySelectorAll(".carousel-inner")[0]
+ while (innerDiv.firstChild) {
+  innerDiv.removeChild(innerDiv.firstChild);
+}
+console.log("out of while loop!")
+console.log(innerDiv)
+})
 
